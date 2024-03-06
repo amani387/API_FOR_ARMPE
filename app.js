692778c9c50a4b78cf18df8ce3ccc64307f7373b
amani387 <negaamanuel387@gmail.com>
2024-03-06 23:12:23 +0300

    the cors module added

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express =require("express");
 const bodyparser =require("body-parser");
 const dotenv =require("dotenv");
+const cors =require('cors')
 dotenv.config();
+const path =require("path")
 const app =express();
 const dbConnect =require("./init/mongodb")
 const { authRoute, categoryRoute, fileRoute, PostRoute, ReviewRouter } =require("./routes")
@@ -13,11 +15,18 @@ const notfound = require("./controllers/notfound");
 //connect to mongodb
 dbConnect();
 //third party middleware 
+app.use(cors({origin:"*"}))
 app.use(express.json({limit:"500mb"}));
 app.use(bodyparser.urlencoded({limit:"500mb",extended:true}));
 app.use(morgan('dev'));
 
 //route section 
+//server static files 
+app.use(express.static('public'))
+//home route
+app.get("/",(req,res)=>{
+res.sendFile(path.join("public","index.html"))
+})
 app.use("/api/v1/auth",authRoute);
 app.use("/api/v1/category",categoryRoute);
 app.use("/api/v1/file",fileRoute);
